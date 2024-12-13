79458cd6ef56dbde004422c93f7a8f546792d23e
Alexander Ursu <alexanderu@crytek.de>
2024-12-13 12:50:57 +0100

    Replaced deprecated hiera_hash with lookup; corrected formatting

--- manifests/base.pp
+++ manifests/base.pp
@@ -6,22 +6,21 @@ class sysctl::base (
   $purge              = false,
   $values             = undef,
   $hiera_merge_values = false,
-  $symlink99          = $::sysctl::params::symlink99,
-  $sysctl_dir         = $::sysctl::params::sysctl_dir,
-  $sysctl_dir_path    = $::sysctl::params::sysctl_dir_path,
-  $sysctl_dir_owner   = $::sysctl::params::sysctl_dir_owner,
-  $sysctl_dir_group   = $::sysctl::params::sysctl_dir_group,
-  $sysctl_dir_mode    = $::sysctl::params::sysctl_dir_mode,
-) inherits ::sysctl::params {
-
+  $symlink99          = $sysctl::params::symlink99,
+  $sysctl_dir         = $sysctl::params::sysctl_dir,
+  $sysctl_dir_path    = $sysctl::params::sysctl_dir_path,
+  $sysctl_dir_owner   = $sysctl::params::sysctl_dir_owner,
+  $sysctl_dir_group   = $sysctl::params::sysctl_dir_group,
+  $sysctl_dir_mode    = $sysctl::params::sysctl_dir_mode,
+) inherits sysctl::params {
   # Hiera support
   if $hiera_merge_values == true {
-    $values_real = hiera_hash('sysctl::base::values', {})
+    $values_real = lookup('sysctl::base::values', { 'default_value' => {}, 'merge' => 'hash' })
   } else {
     $values_real = $values
   }
   if $values_real != undef {
-    create_resources(sysctl,$values_real)
+    create_resources(sysctl, $values_real)
   }
 
   if $sysctl_dir {
@@ -30,6 +29,7 @@ class sysctl::base (
     } else {
       $recurse = false
     }
+
     file { $sysctl_dir_path:
       ensure  => 'directory',
       owner   => $sysctl_dir_owner,
@@ -39,6 +39,7 @@ class sysctl::base (
       purge   => $purge,
       recurse => $recurse,
     }
+
     if $symlink99 and $sysctl_dir_path =~ /^\/etc\/[^\/]+$/ {
       file { "${sysctl_dir_path}/99-sysctl.conf":
         ensure => link,
@@ -48,6 +49,4 @@ class sysctl::base (
       }
     }
   }
-
 }
-
